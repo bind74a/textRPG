3c056eeb35cf284d71f8a661ce3565061afda64d
subin <subin24890@gmail.com>
2025-02-03 13:17:00 +0900

    스탯정보 및 첫번째 메뉴

--- textRPG/Program.cs
+++ textRPG/Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
+        static int level;
+        static string job;
+        static int attack;
+        static int defense
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할수있습니다.");
+
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
+
+            Console.Write("원하시는 행동을 입력새주세요. : ");
+            Console.Read();
         }
+
+
     }
 }
